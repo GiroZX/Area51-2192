083d9f002458bae1f2a1a9f5e37ab193f1563484
Oscar Andrade <ailezx@gmail.com>
2019-11-28 21:53:43 -0500

    Fin de clase 4, Hang Man completado

--- Clases/Clase 2/HangMan/HangMan/Program.cs
+++ Clases/Clase 2/HangMan/HangMan/Program.cs
@@ -12,15 +12,73 @@ namespace HangMan
         {
 
             string secretWord = "pantalones";
+            string tempWord = "";
+            string publicWord = "";
             string letter = "";
+            int hp = 5;
 
-            Console.WriteLine("Ingrese una letra:");
-            letter = Console.ReadLine();
-            Console.WriteLine(">" + letter);
+            for (int i = 0; i < secretWord.Length; i++)
+            {
+                tempWord += "*";
+            }
+            publicWord = tempWord;
+            Console.WriteLine(publicWord);
 
 
+            while (hp > 0)
+            {
+                Console.WriteLine("HP = " + hp);
+                Console.WriteLine("Ingrese una letra:");
+                letter = Console.ReadLine();
+                Console.Clear();
 
-            Console.ReadLine();
+                //Console.WriteLine(">" + letter);
+
+                tempWord = "";
+                /*foreach (char c in secretWord)
+                {
+                    if (letter == "" + c)
+                    {
+                        tempWord += c; // tempWord = tempWord + c;
+                    }
+
+                    else
+                    {
+                        tempWord += "*";
+                    }
+                }*/
+                for (int i = 0; i < secretWord.Length; i++)
+                {
+                    if (letter == "" + secretWord[i])
+                    {
+                        tempWord += letter; //tempWord += secretWord[i];
+                    }
+                    else
+                    {
+                        tempWord += publicWord[i];
+                    }
+                }
+
+                if (publicWord == tempWord)
+                {
+                    hp--;
+                }
+
+                if(secretWord == tempWord)
+                {
+                    Console.WriteLine("You live another day, mortal!");
+                    break;
+                }
+
+                publicWord = tempWord;
+                Console.WriteLine(publicWord);
+            }
+
+
+            if (hp <= 0)
+            { Console.WriteLine("YOUR SOUL IS MINE!!!"); }
+
+                Console.ReadLine();
         }
     }
 }
